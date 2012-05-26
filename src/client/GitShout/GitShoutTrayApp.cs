e8f979bdda2bc9985966b8d0953d09e6c41b1771
alexgit <alex.tashev@gmail.com>
2012-05-26 09:53:09 +0100

    The tray app now saves the last server that was entered.

--- src/client/GitShout/GitShoutTrayApp.cs
+++ src/client/GitShout/GitShoutTrayApp.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.IO;
 
 namespace GitShout
 {
     public partial class GitShoutTrayApp : Form
     {
-        private const string DEFAULT_SERVER = "192.168.0.95";
+        private const string SERVER_CFG_FILE = "serv.cfg";
         private const int DEFAULT_PORT = 9898;
         private const string APP_NAME = "GitShout";
         private const string COULD_NOT_CONNECT_MESSAGE = "Failed to connect to the GitShout server. Check the server name is correct and make sure it accepts connections on port {0}.";
@@ -23,7 +24,7 @@ namespace GitShout
 
             this.messageFormatter = messageFormatter;
 
-            txtServer.Text = DEFAULT_SERVER;
+            txtServer.Text = GetLastServer();
             txtPortNumber.Text = DEFAULT_PORT.ToString();
             chkUseDefaultPort.Checked = true;
 
@@ -32,7 +33,7 @@ namespace GitShout
             trayIcon.ContextMenu = trayMenu;
             trayIcon.Text = APP_NAME;
             trayIcon.BalloonTipClicked += OpenCommitsInBrowser;                  
-        }
+        }       
 
         private void btnConnect_Click(object sender, EventArgs e)
         {
@@ -46,6 +47,7 @@ namespace GitShout
 
             try
             {
+                SaveServer(server);
                 ConnectToServer(server, port);
             }
             catch (Exception ex)
@@ -53,6 +55,32 @@ namespace GitShout
                 MessageBox.Show(this, string.Format(COULD_NOT_CONNECT_MESSAGE, port),
                         "Could not connect to GitShout server.", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);                
             }
+        }
+
+        private void SaveServer(string server)
+        {
+            try
+            {
+                File.WriteAllText(SERVER_CFG_FILE, server);
+            }
+            catch (Exception e) 
+            {
+                CouldntSaveServer(string.Format("({0})", e.Message));
+            }            
+        }
+
+        private string GetLastServer()
+        {
+            string result = "";
+            try { result = File.ReadAllText(SERVER_CFG_FILE); } catch (Exception) { }
+
+            return result;
+        }
+
+        private void CouldntSaveServer(string reason) 
+        {
+            MessageBox.Show(this, string.Format("Couldn't save the the server for next time. {0}", reason),
+                        "Couldn't save server.", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
         }
 
         private void ConnectToServer(string server, int port)
